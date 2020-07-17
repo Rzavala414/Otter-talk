d1b8794a3fc20b7152ae6b107d4773512cbbc8d4
Roger Zavala <rogeliozavala15@gmail.com>
2020-07-17 15:48:58 -0700

    added sign up page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import Signup from './Components/Register/register'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+     <Signup>
+       
+     </Signup>
     </div>
   );
 }
 
 export default App;
+
+
