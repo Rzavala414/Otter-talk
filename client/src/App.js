2f41d5e6cb1b87e4742398f90ac87245587a8a08
Roger Zavala <rogeliozavala15@gmail.com>
2020-07-20 20:10:02 -0700

    changed files for nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Signup from './Pages/register';
-import Login from './Pages/Login/login'
+import Login from './Pages/Login/login';
+import PublicFeed from './Pages/PublicFeed/publicfeed'
 import './App.css';
 
 export default function App() {
   return (
     <Router>
       <>
+        <Route path="/public-feed" component={PublicFeed}></Route>
         <Route exact path='/' component={Login}/>
         <Route exact path='/register' component={Signup}/>
       </>
---
+++ client/src/Pages/PublicFeed/publicfeed.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Nav from '../../Components/Nav/Nav'
+
+export default function PublicFeed() {
+    return (
+        <>
+            <Nav/>
+        </>
+    );
+}
